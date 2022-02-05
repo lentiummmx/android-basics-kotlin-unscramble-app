3facc8c42a6cee9f7661432105fedd65ebcf61de
lentiummmx <lentium.mmx@gmail.com>
2022-02-04 19:45:49 -0600

    solution_livedata_databinding File(s) committed to implementing DataBinding and LiveData persistence from Android Architecture Components (v0.1.0)

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -21,6 +21,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.android.unscramble.R
@@ -47,7 +48,8 @@ class GameFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View {
         // Inflate the layout XML file and return a binding object instance
-        binding = GameFragmentBinding.inflate(inflater, container, false)
+        //binding = GameFragmentBinding.inflate(inflater, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.game_fragment, container, false)
         Log.d(TAG, "GameFragment created/recreated!")
         Log.d(TAG, "Word: ${viewModel.currentScrambledWord} " +
                 "Score: ${viewModel.score} " +
@@ -58,23 +60,30 @@ class GameFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.gameViewModel = viewModel
+        binding.maxNoOfWords = MAX_NO_OF_WORDS
+
+        // Specify the fragment view as the lifecycle owner of the binding
+        // This is used so that binding can observe LiveData updates
+        binding.lifecycleOwner = viewLifecycleOwner
+
         // Setup a click listener for the Submit and Skip buttons.
         binding.submit.setOnClickListener { onSubmitWord() }
         binding.skip.setOnClickListener { onSkipWord() }
 
         // Observe the currentScrambledWord LiveData
-        viewModel.currentScrambledWord.observe(viewLifecycleOwner) { newWord ->
-            binding.textViewUnscrambledWord.text = newWord
-        }
+        //viewModel.currentScrambledWord.observe(viewLifecycleOwner) { newWord ->
+        //    binding.textViewUnscrambledWord.text = newWord
+        //}
         // Update the UI
-        viewModel.score.observe(viewLifecycleOwner) { newScore ->
-            binding.score.text = getString(R.string.score, newScore)
-        }
-        viewModel.currentWordCount.observe(viewLifecycleOwner) { newCount ->
-            binding.wordCount.text = getString(
-                R.string.word_count, newCount, MAX_NO_OF_WORDS
-            )
-        }
+        //viewModel.score.observe(viewLifecycleOwner) { newScore ->
+        //    binding.score.text = getString(R.string.score, newScore)
+        //}
+        //viewModel.currentWordCount.observe(viewLifecycleOwner) { newCount ->
+        //    binding.wordCount.text = getString(
+        //        R.string.word_count, newCount, MAX_NO_OF_WORDS
+        //    )
+        //}
     }
 
     /*
